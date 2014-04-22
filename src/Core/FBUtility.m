73c19418cc0dd0a076b720605dac5e8875a242a1
Bernhard Schandl <bernhard.schandl@gmail.com>
2014-04-22 21:46:17 +0200

    [QUI-105] Remove usage of ASIdentifierManager

--- src/Core/FBUtility.m
+++ src/Core/FBUtility.m
@@ -333,11 +333,6 @@ static NSDate *g_fetchedAppSettingsTimestamp = nil;
 
 + (NSString *)advertiserID {
     NSString *advertiserID = nil;
-    Class ASIdentifierManagerClass = [FBDynamicFrameworkLoader loadClass:@"ASIdentifierManager" withFramework:@"AdSupport"];
-    if ([ASIdentifierManagerClass class]) {
-        ASIdentifierManager *manager = [ASIdentifierManagerClass sharedManager];
-        advertiserID = [[manager advertisingIdentifier] UUIDString];
-    }
     return advertiserID;
 }
 
@@ -346,13 +341,6 @@ static NSDate *g_fetchedAppSettingsTimestamp = nil;
         return AdvertisingTrackingDisallowed;
     }
     FBAdvertisingTrackingStatus status = AdvertisingTrackingUnspecified;
-    Class ASIdentifierManagerClass = [FBDynamicFrameworkLoader loadClass:@"ASIdentifierManager" withFramework:@"AdSupport"];
-    if ([ASIdentifierManagerClass class]) {
-        ASIdentifierManager *manager = [ASIdentifierManagerClass sharedManager];
-        if (manager) {
-            status = [manager isAdvertisingTrackingEnabled] ? AdvertisingTrackingAllowed : AdvertisingTrackingDisallowed;
-        }
-    }
     return status;
 }
 
